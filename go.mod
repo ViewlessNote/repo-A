0afd435761fa279b5b2ff3199b9780d4577ec241
Jonas <jonas.feldhaus@icloud.com>
2026-03-11 15:04:25 +0100

    feat/new-git-thingi

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ViewlessNote/actions-test
+
+go 1.22
+// replace  github.com/ViewlessNote/actions-test => ../sdk
+
+require (
+	google.golang.org/protobuf v1.36.5
+	github.com/ViewlessNote/actions-test v0.1
+)
+
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	greeterv1 "github.com/ViewlessNote/actions-test/gen/go/greeter/v1"
+)
+
+// greet is a simple handler demonstrating the generated SDK types.
+func greet(req *greeterv1.GreetRequest) *greeterv1.GreetResponse {
+	return &greeterv1.GreetResponse{Message: fmt.Sprintf("Marco, %s!", req.GetVorName())}
+}
+
+func main() {
+	req := &greeterv1.GreetRequest{VorName: "Polo"}
+	resp := greet(req)
+	fmt.Println(resp.GetMessage())
+	fmt.Println("The End? ")
+}
